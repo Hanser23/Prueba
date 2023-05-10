fea8a87cb549d442e2b4218ba8a1b079b88330a3
Hanser23 <hanserluis7@gmail.com>
2023-05-09 23:54:58 -0400

    Initial commit

---
+++ Prueba/Models/PruebaBanreservaBdContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Prueba.Models;
+
+public partial class PruebaBanreservaBdContext : DbContext
+{
+    public PruebaBanreservaBdContext()
+    {
+    }
+
+    public PruebaBanreservaBdContext(DbContextOptions<PruebaBanreservaBdContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Persona> Personas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Persona>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Persona__3214EC272371B436");
+
+            entity.ToTable("Persona");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.FechaDeNacimiento).HasColumnType("datetime");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
